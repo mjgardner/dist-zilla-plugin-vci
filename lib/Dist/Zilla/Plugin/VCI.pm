b74c1c08dd998e947400034d062e6554a8e8934a
Mark Gardner <gardnerm@gsicommerce.com>
2011-12-01 15:05:04 -0500

    add description to main VCI module

--- lib/Dist/Zilla/Plugin/VCI.pm
+++ lib/Dist/Zilla/Plugin/VCI.pm
@@ -6,3 +6,9 @@ use utf8;
 1;
 
 # ABSTRACT: Update your VCI-supported repository after release
+
+=head1 DESCRIPTION
+
+This set of plugins for L<Dist::Zilla|Dist::Zilla> will eventually let module
+authors do interesting things using version control systems supported by
+L<VCI|VCI>.
