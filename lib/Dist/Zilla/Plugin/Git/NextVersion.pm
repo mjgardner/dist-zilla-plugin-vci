ac2557737d645396d5beae2a9ae1ad6f0d921076
Mark Gardner <gardnerm@gsicommerce.com>
2011-12-02 15:51:03 -0500

    perlcritic

--- lib/Dist/Zilla/Plugin/Git/NextVersion.pm
+++ lib/Dist/Zilla/Plugin/Git/NextVersion.pm
@@ -6,48 +6,56 @@ use utf8;
 # VERSION
 use Dist::Zilla 4 ();
 use Git::Wrapper;
-use Version::Next ();
+use English '-no_match_vars';
+use Version::Next 'next_version';
 use version 0.80 ();
 
 use Moose;
+use MooseX::Has::Sugar;
+use MooseX::Types::Moose 'Str';
 use namespace::autoclean 0.09;
 
-with 'Dist::Zilla::Role::VersionProvider';
-with 'Dist::Zilla::Role::Git::Repo';
+with qw(Dist::Zilla::Role::VersionProvider Dist::Zilla::Role::Git::Repo);
 
 # -- attributes
 
-has version_regexp => ( is => 'ro', isa => 'Str', default => '^v(.+)$' );
-
-has first_version => ( is => 'ro', isa => 'Str', default => '0.001' );
+my %attr = ( version_regexp => '^v(.+)$', first_version => '0.001' );
+while ( my ( $name, $default ) = each %attr ) {
+    has $name => ( ro, isa => Str, default => $default );
+}
 
 # -- role implementation
 
 sub provide_version {
-    my ($self) = @_;
+    my ($self) = @ARG;
 
     # override (or maybe needed to initialize)
     return $ENV{V} if exists $ENV{V};
 
-    local $/ = "\n";    # Force record separator to be single newline
-
+    local $INPUT_RECORD_SEPARATOR = "\n";
     my $git    = Git::Wrapper->new( $self->repo_root );
     my $regexp = $self->version_regexp;
 
     my @tags = $git->tag;
-    @tags = map { /$regexp/ ? $1 : () } @tags;
-    return $self->first_version unless @tags;
+    {
+        ## no critic (RegularExpressions::RequireDotMatchAnything)
+        ## no critic (RegularExpressions::RequireExtendedFormatting)
+        ## no critic (RegularExpressions::RequireLineBoundaryMatching)
+        @tags = map { /$regexp/ ? $1 : () } @tags;
+    }
+    return $self->first_version if not @tags;
 
     # find highest version from tags
-    my ($last_ver) = sort { version->parse($b) <=> version->parse($a) }
+    my ($last_ver)
+        = reverse sort { version->parse($a) <=> version->parse($b) }
         grep {
-        eval { version->parse($_) }
+        eval { version->parse($ARG) }
         } @tags;
+    if ( !defined $last_ver ) {
+        $self->log_fatal('Could not determine last version from tags');
+    }
 
-    $self->log_fatal("Could not determine last version from tags")
-        unless defined $last_ver;
-
-    my $new_ver = Version::Next::next_version($last_ver);
+    my $new_ver = next_version($last_ver);
     $self->log("Bumping version from $last_ver to $new_ver");
 
     return $self->zilla->version("$new_ver");
@@ -93,7 +101,7 @@ C<version_regexp> - regular expression that matches a tag containing
 a version.  It must capture the version into $1.  Defaults to ^v(.+)$
 which matches the default C<tag_format> from
 L<Dist::Zilla::Plugin::Git::Tag|Dist::Zilla::Plugin::Git::Tag>.
-If you change C<tag_format>, you B<must> set a corresponsing C<version_regexp>.
+If you change C<tag_format>, you B<must> set a corresponding C<version_regexp>.
 
 =back
 
